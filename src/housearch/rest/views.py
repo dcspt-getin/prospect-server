ed122c261d059780e1b1b39175e21499f40306ed
Nuno Silva <nuno4slb@gmail.com>
2022-07-02 09:20:03 +0100

    remove permissions from housearch rest endpoints

--- src/housearch/rest/views.py
+++ src/housearch/rest/views.py
@@ -7,27 +7,17 @@ from .serializers import TerritorialCoverageSerializer, TerritorialUnitSerialize
 from housearch.models import TerritorialCoverage, TerritorialUnit
 
 
-class CustomDjangoModelPermissions(DjangoModelPermissions):
-    def __init__(self):
-        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
-        self.perms_map['POST'] = ['%(app_label)s.add_%(model_name)s']
-        self.perms_map['PUT'] = ['%(app_label)s.change_%(model_name)s']
-        self.perms_map['DELETE'] = ['%(app_label)s.delete_%(model_name)s']
-
-
 # ViewSets define the view behavior.
 class TerritorialCoverageViewSet(viewsets.ModelViewSet):
     queryset = TerritorialCoverage.objects.all()
     serializer_class = TerritorialCoverageSerializer
-    permission_classes = [permissions.IsAuthenticated,
-                          CustomDjangoModelPermissions]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class TerritorialUnitViewSet(viewsets.ModelViewSet):
     queryset = TerritorialUnit.objects.all()
     serializer_class = TerritorialUnitSerializer
-    permission_classes = [permissions.IsAuthenticated,
-                          CustomDjangoModelPermissions]
+    permission_classes = [permissions.IsAuthenticated]
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = {
         'id': ["in", "exact"],  # note the 'in' field
